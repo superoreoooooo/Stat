e2b294182568f7bd7b910d9b64959702b487e9a8
oreo <oooowwww365@gmail.com>
2023-01-04 19:45:51 +0900

    Initial commit

---
+++ src/main/java/win/oreo/stat/Stat.java
@@ -0,0 +1,17 @@
+package win.oreo.stat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
